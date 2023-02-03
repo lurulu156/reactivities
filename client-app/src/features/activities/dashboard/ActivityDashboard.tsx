cfc86eb292fb31b56c2d4acacc5bb166d320684f
lurulu156 <kzzlin9@gmail.com>
2023-02-03 16:30:02 +0800

    feat: add ActivityDetails component

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,5 +1,6 @@
-import { Grid, List } from "semantic-ui-react";
+import { Grid } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
+import ActivityDetails from "../details/ActivityDetails";
 import ActivityList from "./ActivityList";
 
 interface Props {
@@ -10,7 +11,11 @@ export default function ActivityDashboard({ activities }: Props) {
   return (
     <Grid>
       <Grid.Column width='10'>
-        <ActivityList activities={activities}/>
+        <ActivityList activities={activities} />
+      </Grid.Column>
+      <Grid.Column width='6'>
+        {activities[0] &&
+          <ActivityDetails activity={activities[0]} />}
       </Grid.Column>
     </Grid>
   )
