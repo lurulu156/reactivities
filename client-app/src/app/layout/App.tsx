4582c945a41562201cdff8665f377a2eb4b0619b
lurulu156 <kzzlin9@gmail.com>
2023-02-03 15:40:58 +0800

    feat: add navbar component in react

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,28 +1,32 @@
-import React, { useEffect, useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import axios from 'axios';
-import { Header } from 'semantic-ui-react';
+import { Container, Header } from 'semantic-ui-react';
 import List from 'semantic-ui-react/dist/commonjs/elements/List';
+import { Activity } from '../models/activity';
+import NavBar from './NavBar';
 
 function App() {
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useState<Activity[]>([]);
   useEffect(() => {
-    axios.get('http://localhost:5000/api/activities')
+    axios.get<Activity[]>('http://localhost:5000/api/activities')
       .then(response => {
         setActivities(response.data);
       })
   }, [])
 
   return (
-    <div>
-      <Header as='h2' icon='users' content='Reactivities' />
-      <List>
-        {activities.map((activity: any) => (
-          <List.Item key={activity.id}>
-            {activity.title}
-          </List.Item>
-        ))}
-      </List>
-    </div>
+    <Fragment>
+      <NavBar />
+      <Container style={{ marginTop: '7em' }}>
+        <List>
+          {activities.map((activity) => (
+            <List.Item key={activity.id}>
+              {activity.title}
+            </List.Item>
+          ))}
+        </List>
+      </Container>
+    </Fragment>
   );
 }
 
