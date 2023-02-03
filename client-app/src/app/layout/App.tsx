24f2ad72dcfce310f0a8d16c1e84ee6c78b854ed
lurulu156 <kzzlin9@gmail.com>
2023-02-03 17:30:34 +0800

    style: activity form close and open

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -8,7 +8,8 @@ import ActivityDashboard from '../../features/activities/dashboard/ActivityDashb
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
-  
+  const [editMode, setEditMode] = useState(false);
+
   useEffect(() => {
     axios.get<Activity[]>('http://localhost:5000/api/activities')
       .then(response => {
@@ -24,15 +25,27 @@ function App() {
     setSelectedActivity(undefined);
   }
 
+  function handleFormOpen(id?: string) {
+    id ? handleSelectActivity(id) : handleCancelSelectedActivity();
+    setEditMode(true);
+  }
+
+  function handleFormClose() {
+    setEditMode(false);
+  }
+
   return (
     <>
-      <NavBar />
+      <NavBar openForm={handleFormOpen} />
       <Container style={{ marginTop: '7em' }}>
-        <ActivityDashboard 
-        activities={activities} 
-        selectedActivity={selectedActivity}
-        selectActivity={handleSelectActivity}
-        cancelSelectActivity={handleCancelSelectedActivity}
+        <ActivityDashboard
+          activities={activities}
+          selectedActivity={selectedActivity}
+          selectActivity={handleSelectActivity}
+          cancelSelectActivity={handleCancelSelectedActivity}
+          editMode={editMode}
+          openForm={handleFormOpen}
+          closeForm={handleFormClose}
         />
       </Container>
     </>
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,17 +1,23 @@
 import { Button, Form, Segment } from "semantic-ui-react";
+import { Activity } from "../../../app/models/activity";
 
-export default function ActivityForm() {
+interface Props {
+  activity: Activity | undefined;
+  closeForm: () => void;
+}
+
+export default function ActivityForm({ activity, closeForm }: Props) {
   return (
     <Segment clearing>
       <Form>
-        <Form.Input placeholder='Title' />
-        <Form.TextArea placeholder='Description' />
-        <Form.Input placeholder='Category' />
-        <Form.Input placeholder='Date' />
-        <Form.Input placeholder='City' />
-        <Form.Input placeholder='Venue' />
+        <Form.Input placeholder='Title' defaultValue={activity?.title}/>
+        <Form.TextArea placeholder='Description' defaultValue={activity?.description} />
+        <Form.Input placeholder='Category' defaultValue={activity?.category} />
+        <Form.Input placeholder='Date' defaultValue={activity?.date} />
+        <Form.Input placeholder='City' defaultValue={activity?.city} />
+        <Form.Input placeholder='Venue' defaultValue={activity?.venue} />
         <Button floated='right' positive type='submit' content='Submit' />
-        <Button floated='right' type='button' content='Cancel' />
+        <Button onClick={closeForm} floated='right' type='button' content='Cancel' />
       </Form>
     </Segment>
   )
