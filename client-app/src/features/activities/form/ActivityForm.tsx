f0e9cde877278d0444bc55e0c8d8b954ae526271
lurulu156 <kzzlin9@gmail.com>
2023-02-04 17:39:04 +0800

    feat: CRUD for react to API server

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -6,9 +6,10 @@ interface Props {
   activity: Activity | undefined;
   closeForm: () => void;
   createOrEdit: (activity: Activity) => void;
+  submitting: boolean;
 }
 
-export default function ActivityForm({ activity: selectedActivity, closeForm, createOrEdit }: Props) {
+export default function ActivityForm({ activity: selectedActivity, closeForm, createOrEdit, submitting }: Props) {
   const initialState = selectedActivity ?? {
     id: '',
     title: '',
@@ -38,7 +39,7 @@ export default function ActivityForm({ activity: selectedActivity, closeForm, cr
         <Form.Input placeholder='Date' type='date' defaultValue={activity.date} name='date' onChange={handleInputChange} />
         <Form.Input placeholder='City' defaultValue={activity.city} name='city' onChange={handleInputChange} />
         <Form.Input placeholder='Venue' defaultValue={activity.venue} name='venue' onChange={handleInputChange} />
-        <Button floated='right' positive type='submit' content='Submit' />
+        <Button loading={submitting} floated='right' positive type='submit' content='Submit' />
         <Button onClick={closeForm} floated='right' type='button' content='Cancel' />
       </Form>
     </Segment>
