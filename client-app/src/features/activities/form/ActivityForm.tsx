0a0636d41877e2c77626fa4898b74607247ed230
lurulu156 <kzzlin9@gmail.com>
2023-02-06 14:25:51 +0800

    feat: add router and navigate logic

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,12 +1,18 @@
 import { observer } from "mobx-react-lite";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { Button, Form, Segment } from "semantic-ui-react";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
+import { Activity } from "../../../app/models/activity";
 import { useStore } from "../../../app/stores/store";
+import { v4 as uuid } from 'uuid';
 
 export default observer(function ActivityForm() {
   const { activityStore } = useStore();
-  const { selectedActivity, closeForm, createActivity, updateActivity, loading } = activityStore
-  const initialState = selectedActivity ?? {
+  const { createActivity, updateActivity, loading, loadActivity, loadingInitial } = activityStore;
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [activity, setActivity] = useState<Activity>({
     id: '',
     title: '',
     category: '',
@@ -14,17 +20,27 @@ export default observer(function ActivityForm() {
     date: '',
     city: '',
     venue: ''
-  };
-  const [activity, setActivity] = useState(initialState);
+  });
+
+  useEffect(() => {
+    if (id) loadActivity(id).then(activity => setActivity(activity!))
+  }, [id, loadActivity]);
 
   function handleSubmit() {
-    activity.id ? updateActivity(activity) : createActivity(activity)
+    if (!activity.id) {
+      activity.id = uuid();
+      createActivity(activity).then(() => navigate(`/activities/${activity.id}`));
+    } else {
+      updateActivity(activity).then(() => navigate(`/activities/${activity.id}`));
+    }
   }
 
   function handleInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
     const { name, value } = event.target;
     setActivity({ ...activity, [name]: value })
   }
+
+  if (loadingInitial) return <LoadingComponent content='Loading activity...' />
 
   return (
     <Segment clearing>
@@ -36,7 +52,7 @@ export default observer(function ActivityForm() {
         <Form.Input placeholder='City' defaultValue={activity.city} name='city' onChange={handleInputChange} />
         <Form.Input placeholder='Venue' defaultValue={activity.venue} name='venue' onChange={handleInputChange} />
         <Button loading={loading} floated='right' positive type='submit' content='Submit' />
-        <Button onClick={closeForm} floated='right' type='button' content='Cancel' />
+        <Button as={Link} to='/activities' floated='right' type='button' content='Cancel' />
       </Form>
     </Segment>
   )
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,19 +1,17 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'semantic-ui-css/semantic.min.css';
 import './app/layout/styles.css';
-import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 import { store, StoreContex } from './app/stores/store';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './app/router/Routes';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <StoreContex.Provider value={store}>
-    {/*  <React.StrictMode> */}
-    <App />
-    {/*  </React.StrictMode> */}
+    <RouterProvider router={router} />
   </StoreContex.Provider>
 );
 
