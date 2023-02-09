8d965cb520c1b4f4965dd9a7fa5fe151653c7f93
lurulu156 <kzzlin9@gmail.com>
2023-02-09 10:35:53 +0800

    feat: add register and validation for register

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -1,10 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.DTOs
 {
   public class RegisterDto
   {
-    public string Email { get; set; }
-    public string Password { get; set; }
+    [Required]
     public string DisplayName { get; set; }
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+    [Required]
+    [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}", ErrorMessage = "Password must include lowercase, uppercase, number and has length of 4-8")]
+    public string Password { get; set; }
+    [Required]
     public string Username { get; set; }
   }
 }
