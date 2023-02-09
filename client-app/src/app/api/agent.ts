5bdd87afc79225f8018dd3c79c33764489d6d96e
lurulu156 <kzzlin9@gmail.com>
2023-02-09 14:55:14 +0800

    feat: token store and take and validation handling

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosError, AxiosResponse } from "axios";
 import { toast } from 'react-toastify';
 import { Activity } from "../models/activity";
+import { User, UserFormValues } from "../models/user";
 import { router } from "../router/Routes";
 import { store } from "../stores/store";
 
@@ -12,6 +13,12 @@ const sleep = (delay: number) => {
 
 axios.defaults.baseURL = 'http://localhost:5000/api';
 
+axios.interceptors.request.use(config => {
+  const token = store.commonStore.token;
+  if (token && config.headers) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+})
+
 axios.interceptors.response.use(async response => {
   await sleep(1000);
   return response;
@@ -19,7 +26,7 @@ axios.interceptors.response.use(async response => {
   const { data, status, config } = error.response as AxiosResponse;
   switch (status) {
     case 400:
-      if(config.method === 'get' && data.errors.hasOwnProperty('id')) {
+      if (config.method === 'get' && data.errors.hasOwnProperty('id')) {
         router.navigate('/not-found');
       }
       if (data.errors) {
@@ -68,8 +75,15 @@ const Activities = {
   delete: (id: string) => requests.del<void>(`/activities/${id}`)
 }
 
+const Account = {
+  current: () => requests.get<User>('/account'),
+  login: (user: UserFormValues) => requests.post<User>('/account/login', user),
+  register: (user: UserFormValues) => requests.post<User>('/account/register', user)
+}
+
 const agent = {
-  Activities
+  Activities,
+  Account
 }
 
 export default agent;
