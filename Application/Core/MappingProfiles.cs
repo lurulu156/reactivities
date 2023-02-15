dd2440aab2c7abf9603804ef853c2eaf23472d47
lurulu156 <kzzlin9@gmail.com>
2023-02-15 11:38:38 +0800

    feat: get list of follower and add mapper

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -13,13 +13,18 @@ namespace Application.Core
       CreateMap<Activity, ActivityDto>()
       .ForMember(d => d.HostUsername, option =>
       option.MapFrom(source => source.Attendees.FirstOrDefault(x => x.IsHost).AppUser.UserName));
+
       CreateMap<ActivityAttendee, AttendeeDto>()
       .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.AppUser.DisplayName))
       .ForMember(d => d.Username, o => o.MapFrom(s => s.AppUser.UserName))
       .ForMember(d => d.Bio, o => o.MapFrom(s => s.AppUser.Bio))
       .ForMember(d => d.Image, o => o.MapFrom(s => s.AppUser.Photos.FirstOrDefault(x => x.isMain).Url));
+
       CreateMap<AppUser, Profiles.Profile>()
-      .ForMember(d => d.Image, o => o.MapFrom(s => s.Photos.FirstOrDefault(x => x.isMain).Url));
+      .ForMember(d => d.Image, o => o.MapFrom(s => s.Photos.FirstOrDefault(x => x.isMain).Url))
+      .ForMember(d => d.FollowersCount, o => o.MapFrom(s => s.Followers.Count))
+      .ForMember(d => d.FollowingCount, o => o.MapFrom(s => s.Followings.Count));
+
       CreateMap<Comment, CommentDto>()
       .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.Author.DisplayName))
       .ForMember(d => d.Username, o => o.MapFrom(s => s.Author.UserName))
