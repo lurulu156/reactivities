642c2c74dd326b0d1816a647197d02b13a25280b
lurulu156 <kzzlin9@gmail.com>
2023-02-15 15:53:52 +0800

    feat: view following list at profile

--- client-app/src/features/profiles/ProfileContent.tsx
+++ client-app/src/features/profiles/ProfileContent.tsx
@@ -1,19 +1,20 @@
 import { observer } from "mobx-react-lite";
 import { Tab } from "semantic-ui-react";
 import { Profile } from "../../app/models/profile";
+import ProfileFollowings from "./ProfileFollowings";
 import ProfilePhotos from "./ProfilePhotos";
 
 interface Props {
   profile: Profile;
 }
 
-export default observer(function ProfileContent({profile}: Props) {
+export default observer(function ProfileContent({ profile }: Props) {
   const panes = [
     { menuItem: 'About', render: () => <Tab.Pane>About Content</Tab.Pane> },
-    { menuItem: 'Photos', render: () => <ProfilePhotos profile={profile}/> },
+    { menuItem: 'Photos', render: () => <ProfilePhotos profile={profile} /> },
     { menuItem: 'Events', render: () => <Tab.Pane>Events Content</Tab.Pane> },
-    { menuItem: 'Followers', render: () => <Tab.Pane>Followers Content</Tab.Pane> },
-    { menuItem: 'Following', render: () => <Tab.Pane>Following Content</Tab.Pane> },
+    { menuItem: 'Followers', render: () => <ProfileFollowings /> },
+    { menuItem: 'Following', render: () => <ProfileFollowings /> },
   ];
   return (
     <Tab
