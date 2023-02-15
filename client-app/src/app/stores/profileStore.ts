e9944dcc0d09bab589d352fd5a2ba7eb928593f7
lurulu156 <kzzlin9@gmail.com>
2023-02-15 17:57:52 +0800

    feat: add function for edit profile

--- client-app/src/app/stores/profileStore.ts
+++ client-app/src/app/stores/profileStore.ts
@@ -150,4 +150,22 @@ export default class ProfileStore {
       runInAction(() => this.loadingFollowings = false);
     }
   }
+
+  updateProfile = async (profile: Partial<Profile>) => {
+    this.loading = true;
+    try {
+      await agent.Profiles.updateProfile(profile);
+      runInAction(() => {
+        if (profile.displayName && profile.displayName !==
+          store.userStore.user?.displayName) {
+          store.userStore.setDisplayName(profile.displayName);
+        }
+        this.profile = { ...this.profile, ...profile as Profile };
+        this.loading = false;
+      })
+    } catch (error) {
+      console.log(error);
+      runInAction(() => this.loading = false);
+    }
+  }
 }
--- client-app/src/app/stores/userStore.ts
+++ client-app/src/app/stores/userStore.ts
@@ -55,4 +55,8 @@ export default class UserStore {
     if (this.user) this.user.image = image;
     store.activityStore.loadActivities();
   }
+
+  setDisplayName = (name: string) => {
+    if (this.user) this.user.displayName = name;
+  }
 }
