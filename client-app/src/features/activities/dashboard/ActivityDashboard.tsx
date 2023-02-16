8bf2cd78a2e6174c3aaf2f792e4c64815a9f8d86
lurulu156 <kzzlin9@gmail.com>
2023-02-16 15:00:58 +0800

    feat: add infinite scrolling to replace button for more activities

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,6 +1,7 @@
 import { observer } from "mobx-react-lite";
 import { useEffect, useState } from "react";
-import { Button, Grid } from "semantic-ui-react";
+import InfiniteScroll from "react-infinite-scroller";
+import { Grid, Loader } from "semantic-ui-react";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
 import { PagingParams } from "../../../app/models/pagination";
 import { useStore } from "../../../app/stores/store";
@@ -27,11 +28,20 @@ export default observer(function ActivityDashboard() {
   return (
     <Grid>
       <Grid.Column width='10'>
-        <ActivityList />
-        <Button onClick={handleGetNext} content='more...' loading={loadingNext}/>
+        <InfiniteScroll
+          pageStart={0}
+          loadMore={handleGetNext}
+          hasMore={!loadingNext && !!pagination && pagination.currentPage < pagination.totalPages}
+          initialLoad={false}
+        >
+          <ActivityList />
+        </InfiniteScroll>
       </Grid.Column>
       <Grid.Column width='6'>
         <ActivityFilters />
+      </Grid.Column>
+      <Grid.Column width='10'>
+        <Loader active={loadingNext} />
       </Grid.Column>
     </Grid>
   )
